fa0b408b6ca7f8a4633185ca4828150d92a9ce09
Federico Miras <miras.federico@gmail.com>
2018-09-25 17:19:39 -0300

    test: add more cases

--- test/semanticPhase.spec.ts
+++ test/semanticPhase.spec.ts
@@ -1,13 +1,14 @@
 import { expect } from 'chai'
+
 import { ParsingPhaseResult } from '../src/phases/parsingPhase'
 import { CanonicalPhaseResult } from '../src/phases/canonicalPhase'
-import { validateScene } from '../src/phases/semanticPhase'
+import { SemanticPhaseResult } from '../src/phases/semanticPhase'
 import { Node } from '../src/nodes'
 
 const validScene = `
 <scene>
-  <box color="#ff00aa" position="1 2 3" />
-  <sphere color="#00aaff" position="1 2 3" scale="4 4 4" />
+  <box id="3" color="#ff00aa" position="1 2 3" />
+  <sphere id="2" color="#00aaff" position="1 2 3" scale="4 4 4" />
   <plane color="#00aaff" position="1 2 3" scale="3" rotation="-90 0 0" />
 </scene>
 `
@@ -16,7 +17,6 @@ function getErrors(nodes: Node[]): Error[] {
   if (nodes.length === 0) {
     return []
   }
-
   const errors = nodes
     .filter(node => node.errors)
     .map(node => node.errors)
@@ -37,62 +37,98 @@ describe('semanticPhase', () => {
   it('should parse valid scene', () => {
     const parsePhase = new ParsingPhaseResult('file.xml', validScene)
     const canonicalPhase = new CanonicalPhaseResult(parsePhase)
-    const { document } = validateScene(canonicalPhase.document)
+    const { document } = new SemanticPhaseResult(canonicalPhase)
     const errors = getErrors([document])
-    expect(errors.length).to.eq(0)
+    expect(errors.length, `Existing errors: [${errors}]`).to.eq(0)
   })
 
   it(`should add an error if starting tag isn't scene`, () => {
     const parsePhase = new ParsingPhaseResult('file.xml', `<tag><box position="1 2 3" /></tag>`)
     const canonicalPhase = new CanonicalPhaseResult(parsePhase)
-    const { document } = validateScene(canonicalPhase.document)
+    const { document } = new SemanticPhaseResult(canonicalPhase)
     const errors = getErrors([document])
-    expect(errors.length).to.eq(2)
+    expect(errors.length, `Existing errors: [${errors}]`).to.eq(2)
     expect(errors[0].message).to.eq('Type error: Invalid document. Scene must start and finish with <scene> tag.')
   })
 
   it('should add an error to non-existing tag names', () => {
     const parsePhase = new ParsingPhaseResult('file.xml', `<scene><ultrabox /></scene>`)
     const canonicalPhase = new CanonicalPhaseResult(parsePhase)
-    const { document } = validateScene(canonicalPhase.document)
+    const { document } = new SemanticPhaseResult(canonicalPhase)
     const errors = getErrors([document])
-    expect(errors.length).to.eq(1)
+    expect(errors.length, `Existing errors: [${errors}]`).to.eq(1)
     expect(errors[0].message).to.eq(`Type error: Tag <ultrabox> does not exist.`)
-  })
-
-  it('should add an error to invalid attributes', () => {
-    const parsePhase = new ParsingPhaseResult('file.xml', `<scene><box hola="hola"/></scene>`)
-    const canonicalPhase = new CanonicalPhaseResult(parsePhase)
-    const { document } = validateScene(canonicalPhase.document)
-    const errors = getErrors([document])
-    expect(errors.length).to.eq(1)
-    expect(errors[0].message).to.eq(`Type error: attribute "hola" is not valid.`)
   })
 
   it('should add an error to wrong vector values', () => {
     const parsePhase = new ParsingPhaseResult('file.xml', `<scene><box position="hola"/></scene>`)
     const canonicalPhase = new CanonicalPhaseResult(parsePhase)
-    const { document } = validateScene(canonicalPhase.document)
+    const { document } = new SemanticPhaseResult(canonicalPhase)
     const errors = getErrors([document])
-    expect(errors.length).to.eq(1)
-    expect(errors[0].message).to.eq('Type error: Invalid attribute position. Must be Vector3Component type')
+    expect(errors.length, `Existing errors: [${errors}]`).to.eq(1)
+    expect(errors[0].message).to.eq('Invalid attribute position. Must be Vector3Component type.')
   })
 
   it('should add an error to wrong color values', () => {
     const parsePhase = new ParsingPhaseResult('file.xml', `<scene><box color="asd" position="1 2 3" /></scene>`)
     const canonicalPhase = new CanonicalPhaseResult(parsePhase)
-    const { document } = validateScene(canonicalPhase.document)
+    const { document } = new SemanticPhaseResult(canonicalPhase)
     const errors = getErrors([document])
-    expect(errors.length).to.eq(1)
-    expect(errors[0].message).to.eq('Type error: Invalid attribute color. Must be hex number color type')
+    expect(errors.length, `Existing errors: [${errors}]`).to.eq(1)
+    expect(errors[0].message).to.eq('Invalid attribute color. Must be hex number color type.')
   })
 
   it('should add an error to wrong scale values', () => {
     const parsePhase = new ParsingPhaseResult('file.xml', `<scene><sphere color="#00aaff" position="1 2 3" scale="4 4" /></scene>`)
     const canonicalPhase = new CanonicalPhaseResult(parsePhase)
-    const { document } = validateScene(canonicalPhase.document)
+    const { document } = new SemanticPhaseResult(canonicalPhase)
     const errors = getErrors([document])
-    expect(errors.length).to.eq(1)
-    expect(errors[0].message).to.eq('Type error: Invalid attribute scale. Must be 1 digit number or a vector type')
+    expect(errors.length, `Existing errors: [${errors}]`).to.eq(1)
+    expect(errors[0].message).to.eq('Invalid attribute scale. Must be as number or a Vector3Component type.')
+  })
+
+  it('should add an error to scene with non-uniq IDs', () => {
+    const scene = `
+      <scene>
+        <box id="3" color="#ff00aa" position="1 2 3" />
+        <sphere id="3" color="#00aaff" position="1 2 3" scale="4 4 4" />
+        <plane color="#00aaff" position="1 2 3" scale="3" rotation="-90 0 0" />
+      </scene>`
+    const parsePhase = new ParsingPhaseResult('file.xml', scene)
+    const canonicalPhase = new CanonicalPhaseResult(parsePhase)
+    const { document } = new SemanticPhaseResult(canonicalPhase)
+    const errors = getErrors([document])
+    expect(errors.length, `Existing errors: [${errors}]`).to.eq(1)
+    expect(errors[0].message).to.eq('Invalid attribute id. Value must be uniq and is duplicated on this document.')
+  })
+
+  it('should add an error to wrong boolean values', () => {
+    const parsePhase = new ParsingPhaseResult(
+      'file.xml',
+      `<scene><sphere with-collisions="asd" color="#00aaff" position="1 2 3" /></scene>`
+    )
+    const canonicalPhase = new CanonicalPhaseResult(parsePhase)
+    const { document } = new SemanticPhaseResult(canonicalPhase)
+    const errors = getErrors([document])
+    expect(errors.length, `Existing errors: [${errors}]`).to.eq(1)
+    expect(errors[0].message).to.eq('Invalid attribute with-collisions. Must be boolean value.')
+  })
+
+  it('should add an error to wrong billboard values', () => {
+    const parsePhase = new ParsingPhaseResult('file.xml', `<scene><sphere billboard="9" color="#00aaff" position="1 2 3" /></scene>`)
+    const canonicalPhase = new CanonicalPhaseResult(parsePhase)
+    const { document } = new SemanticPhaseResult(canonicalPhase)
+    const errors = getErrors([document])
+    expect(errors.length, `Existing errors: [${errors}]`).to.eq(1)
+    expect(errors[0].message).to.eq('Invalid attribute billboard. Must be number between 0 and 7.')
+  })
+
+  it('should add an error to wrong alignment values', () => {
+    const parsePhase = new ParsingPhaseResult('file.xml', `<scene><text v-align="near" /></scene>`)
+    const canonicalPhase = new CanonicalPhaseResult(parsePhase)
+    const { document } = new SemanticPhaseResult(canonicalPhase)
+    const errors = getErrors([document])
+    expect(errors.length, `Existing errors: [${errors}]`).to.eq(1)
+    expect(errors[0].message).to.eq('Invalid attribute v-align. Must be `top`, `right`, `bottom` or `left`.')
   })
 })
